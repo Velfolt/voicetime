7351a50bbc2d6b61590934b48c6ef58b288194bb
Henrik Lundberg <henrik.lundberg@zenon.se>
2016-03-08 17:08:43 +0100

    Add verbose flag and borrow more

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,21 @@ use voicetime::connection::Connection;
 
 fn main() {
     let mut address = "".to_string();
+    let mut verbose = false;
     {
         let mut ap = argparse::ArgumentParser::new();
         ap.set_description("Voice communication. Becomes the host if --address is missing.");
         ap.refer(&mut address)
             .add_option(&["--address"], argparse::Store, "Address to connect to");
+        ap.refer(&mut verbose)
+            .add_option(&["-v", "--verbose"], argparse::StoreTrue, "Verbose printouts");
         ap.parse_args_or_exit();
     }
 
     if address.len() > 0 {
         let connection = Connection::new();
         let address = &address[..];
-        connection.send_to("tjena".as_bytes(), address);
+        connection.send_to("tjena".as_bytes(), &address);
     } else {
         let connection = Connection::new();
         let local_addr = connection.addr();
@@ -28,7 +31,7 @@ fn main() {
             let (length, addr) = connection.recv_from(&mut buf);
             let bytes_to_string = std::str::from_utf8(&buf[..length]).unwrap();
 
-            println!("{} -> {}", addr, bytes_to_string);
+            println!("{} -> {}", &addr, &bytes_to_string);
         }
     }
 }
